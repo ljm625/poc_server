bdeb615f2828b2c373dfb665bac7d3103591ecd8
Jiaming Li <ljm625@gmail.com>
2018-07-30 18:08:29 +0800

    updated cloudcenter apis

--- modules/CloudCenterRest.py
+++ modules/CloudCenterRest.py
@@ -1,5 +1,7 @@
 import requests
+import yaml
 
+config=yaml.load(open('config.yaml', 'r'))
 
 class CloudCenterControl(object):
     def __init__(self,hostname,username,password):
@@ -9,15 +11,37 @@ class CloudCenterControl(object):
         pass
 
     def build_url(self,url):
-        return "https://{}/api/v1/{}".format(self.hostname,url)
+        return "https://{}:9443/v1/{}".format(self.hostname,url)
 
 
     def get(self,url, header = {}):
-        requests.get()
+        resp = requests.get(self.build_url(url),auth=(self.username, self.password),headers=header)
+        if resp.status_code>300:
+            resp.raise_for_status()
+        else:
+            return resp.json()
 
     def post(self,url,body,header = {"Content-Type":"application/json"}):
-        requests.post()
+        resp=requests.post(self.build_url(url),json=body,headers = header,auth=(self.username, self.password))
+        if resp.status_code>300:
+            resp.raise_for_status()
+        else:
+            return resp.json()
+
+
 
     def delete(self,url,header = {}):
-        requests.delete()
+        resp=requests.delete(self.build_url(url),headers=header,auth = (self.username,self.password))
+        if resp.status_code>300:
+            resp.raise_for_status()
+        else:
+            return resp.json()
 
+
+    def create_instance(self):
+        result = self.post("jobs",config["cloudcenter_profile"])
+        return result
+
+    def get_instance_by_id(self,id):
+        result = self.get("jobs/{}".format(id))
+        return result
